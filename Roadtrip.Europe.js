a7e6cdec245f5e5a23da37e5a5d0bd68cad81c96
NIghel123 <nickel.paulsen@gmail.com>
2018-12-06 19:42:21 +0100

    switched from hbs to express-handlebars and added some pictures

--- Roadtrip.Europe.js
+++ Roadtrip.Europe.js
@@ -2,8 +2,8 @@
 var express = require('express'),
     // we will need both the express-framework and the app
     // so we put them in different variables.
-    app = express();
-// we now want to set up our view engine. We choose handlebars!
+    app = express(),
+    handlebars = require('express-handlebars');
 
 /* define where out website will run locally.
  * the process.env is an object created by nodejs to mirror the enviroment
@@ -11,17 +11,45 @@ var express = require('express'),
  */
 app.set('port', process.env.PORT || 3000);
 
-/* the following is called a view engine! It gives us the possibility to template our html files,
- * i.e. we can but each single page of our site into the same main.hbs file. This allows us to write
- * out code only at ONE place! And not to write it in different places!
+/* next we define our view engine. It allows us to load a single 'surrounding' html page
+ * as out 'layout' and to only put the content that changes into the different webpages
+ * in a nutshell it saves us a lot of writing and the code is only defined once for all pages! 
+ * So that gives us an advantage in maintainability!
+ *
+ *
  */
-app.set('view engine', 'hbs');
+handlebars = handlebars.create({
+    defaultLayout: 'main',
+    extname: '.hbs',
+    helpers: {
+        section: function(name, options) {
+            //most of the time this._sections is undefined!
+            if (!this._sections) this._sections = {};
+            //options.fn(this) is the content between the {{#section 'name'}} content 
+            //{{/section}} placeholders inside the layout file! 
+            //The property '_sections.name' will be the one rendered inside the 
+            //{{{_sections.name}}} placeholder in the main.hbs!
+            this._sections[name] = options.fn(this);
+            return null;
+        }
+    }
+});
+app.engine('.hbs', handlebars.engine);
+app.set('view engine', '.hbs');
+
+/* now before we define any routes we need to define where node normally looks to get
+ * css and javascript and libraries that are on the server. We do that by defining a
+ * standard route to look up sources:
+ */
+app.use(express.static(__dirname + '/public'));
+/* the standard folder is the public folder now. So any path inside our scripts starts at this foler!
+ * thus it is not a relative path anymore!
+ */
 
 /*=================================ROUTES==================================
  * we set our routing, i.e the paths where the browser will find the pages
  * of the website. */
 app.get('/', function(req, res) {
-    res.status(404);
     res.render('home');
 });
 
@@ -40,8 +68,8 @@ app.use(function(req, res, next) {
 //This function will be used when there was an Javascript-Error on the server!
 app.use(function(err, req, res, next) {
     console.error(err.stack);
-    res.status(500);
-    res.render('500');
+    res.status(505);
+    res.render('505');
 });
 
 /*=================================SERVER==================================
