3a3d6e2560c786d63df3e3e7a3f3520ced22e3df
NIghel123 <nickel.paulsen@gmail.com>
2018-12-29 22:55:41 +0100

    improved video, dynamic grid layout, modularized langauage file

--- Roadtrip.Europe.js
+++ Roadtrip.Europe.js
@@ -3,8 +3,9 @@ var express = require('express'),
     // we will need both the express-framework and the app
     // so we put them in different variables.
     app = express(),
-    handlebars = require('express-handlebars');
-
+    handlebars = require('express-handlebars'),
+    languages = require('./lib/languages.js');
+var getPage = languages.setLanguage('de');
 /* define where out website will run locally.
  * the process.env is an object created by nodejs to mirror the enviroment
  * i.e. the computer or server, where it is beeing executed. 
@@ -64,34 +65,11 @@ app.get('/start', function(req, res) {
         }
     );
 });
-app.get('/', function(req, res) {
+app.get(getPage('home').route, function(req, res) {
     res.render('home',
         /*the content of the buttons of the sidebar (from top to bottom)*/
         {
-            /*MAYBE IT IS A GOOD IDEA TO EXPORT THIS KIND OF 'PRIMARY CONTENT' INTO SOME GENERAL FILE!
-             * THAT CAN ALSO SWITCH BETWEEN LANGUAGES! SUCH THAT IT IS EASY TO PROVIDE THE RIGHT
-             * LANGUAGE NEEDED!!! {home:
-             * {de:[{button1:'Mache deinen ...',link1:'/createRoadtrip'},{button2:'...,...},...],
-             * en:[{button1:'Make your own Roadtrip',link...
-             * Or maybe structure it seperately for text and links. Because the links will be the
-             * same anyway in every language! Or could I even change that? Well I think so!*/
-            context: [{
-                    text: 'Mache deinen<br>Traumroadtrip',
-                    link: '/createRoadtrip'
-                },
-                {
-                    text: 'Mitfahrer für<br>Roadtrips gesucht',
-                    link: '/viewRoadtrips'
-                },
-                {
-                    text: 'Was ist das<br>für eine Seite?',
-                    link: '/whatSiteIsThis'
-                },
-                {
-                    text: 'Einloggen',
-                    link: '/login'
-                },
-            ]
+            context: getPage('home').content
         }
     );
 });
